d778ee1e880c167c51e24e4f31b35ff187d606b4
1Zarkos1 <zarkos777@gmail.com>
2020-02-03 20:27:36 +0700

    implemented main calculation function

--- pyqt_gui/calc.py
+++ pyqt_gui/calc.py
@@ -33,14 +33,14 @@ class MyWindow(QMainWindow):
         vertLayout = QVBoxLayout(cenWidget)
         gridWidget = QWidget()
         gridLayout = QGridLayout(gridWidget)
-        self.answerField = QLabel(parent=cenWidget)
+        self.hintField = QLabel(parent=cenWidget)
         self.textField = QLineEdit()
         f = self.textField.font()
         f.setPointSize(18)
         self.textField.setFont(f)
         gridWidget.setLayout(gridLayout)
         # add widgets to top level layout
-        vertLayout.addWidget(self.answerField)
+        vertLayout.addWidget(self.hintField)
         vertLayout.addWidget(self.textField)
         vertLayout.addWidget(gridWidget)
         # add buttons to grid layout
@@ -52,15 +52,44 @@ class MyWindow(QMainWindow):
         self.setCentralWidget(cenWidget)
 
     def doMath(self, buttonValue):
+        currentValue = self.textField.text()
         if buttonValue == 'c':
             self.operand = ''
             self.operator = ''
             self.textField.setText('')
-        elif buttonValue in self.operators:
+        elif buttonValue == '<-' and currentValue != '':
+            if len(currentValue) == 1:
+                self.textField.setText('')
+            else:
+                self.textField.setText(currentValue[:-1])
+        elif (buttonValue in self.operators
+              and self.convertValue(currentValue) is not None):
             self.operator = buttonValue
-        else:
-            self.textField.setText(self.textField.text()+buttonValue)
-        self.answerField.setText(f'<h1>{self.operand + self.operator}</h1>')
+            self.operand = self.convertValue(currentValue)
+            self.textField.setText('')
+        elif buttonValue == '.':
+            if '.' not in currentValue:
+                self.textField.setText(currentValue+buttonValue)
+        elif (buttonValue == '=' and self.operator 
+              and self.convertValue(currentValue) != None):
+            self.textField.setText(
+                f'''{self.operators[self.operator](
+                   self.operand, self.convertValue(currentValue))}''')
+            self.operand = ''
+            self.operator = ''
+        elif self.convertValue(buttonValue) != None:
+            self.textField.setText(currentValue+buttonValue)
+        self.hintField.setText(f'<h1><font color=#a0a0a0>{self.operand}' 
+                                 f'{self.operator}</font></h1>')
+
+    def convertValue(self, value):
+        try:
+            return int(value)
+        except ValueError:
+            try:
+                return float(value)
+            except:
+                return None
 
 
 app = QApplication(sys.argv)
