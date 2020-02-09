82cc1261889388a1f53a0f4329d12bbfbbe0fd1c
1Zarkos1 <zarkos777@gmail.com>
2020-02-09 19:25:19 +0700

    added comments to code, implemented adjacent cell opening if number of adjacent flag marked mines = current button number, added mine counter underneath grid buttons

--- pyqt_gui/board.py
+++ pyqt_gui/board.py
@@ -4,11 +4,11 @@ import pprint
 
 class BoardSet():
 
-    def __init__(self, n_cols, n_rows, n_fact):
+    def __init__(self, n_cols, n_rows, mine_ratio):
         self.n_cols = n_cols
         self.n_rows = n_rows
         self.n_cells = n_cols*n_rows
-        self.n_mines = int(n_fact * self.n_cells)
+        self.n_mines = int(mine_ratio * self.n_cells)
         self.make_set(self.n_cols, self.n_cells, self.n_mines)
 
     def make_set(self, n_cols, n_cells, n_mines):
@@ -17,12 +17,14 @@ class BoardSet():
             '*' if i in rand_sam else '-' for i in range(n_cells)]
         self.split_board = self.make_split_board(self.flat_board)
 
+    # split list into 2 dimensional list to represent rows
     def make_split_board(self, board_list):
         return [
             board_list[i: i+self.n_cols]
             for i in range(0, len(board_list), self.n_cols)
         ]
 
+    # get cells directly around cell with index 'n'
     def get_adjacent_cells(self, n, whole_board):
             col = n % self.n_cols
             row = n//self.n_rows
@@ -42,12 +44,15 @@ class BoardSet():
 
             return comb_list
 
+    # get and return cell value reperesenting number of mines directly adjacent
+    # to chosen cell
     def get_cell_value(self, n):
         if self.flat_board[n] == '*':
             return '*'
         else:
             return str(self.get_adjacent_cells(n, self.split_board).count('*'))
 
+    # for debugging purposes
     def get_set(self):
         open_set = list(map(self.get_cell_value, range(self.n_cells)))
         open_set = [open_set[i: i+self.n_cols]
